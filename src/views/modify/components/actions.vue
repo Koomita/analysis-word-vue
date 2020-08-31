39d15f180654b51f720901b2aaac986bfacc2c42
koomita <koomita@163.com>
2020-08-31 15:58:32 +0800

    fix: 题目过多过长scroll

--- src/views/modify/components/actions.vue
+++ src/views/modify/components/actions.vue
@@ -25,17 +25,16 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-.ant-affix {
-  overflow-y: scroll;
-}
 .box {
   background: #fff;
-  max-height: 100%;
+  max-height: 600px;
+  overflow-y: hidden;
   .case {
     padding: 10px 24px;
     box-sizing: border-box;
     background: #fff;
-    overflow: scroll;
+    max-height: 100%;
+    overflow-y: scroll;
     .step-flow {
       .action-title {
         h1 {
