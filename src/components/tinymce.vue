992f9f9335e5615aa42e2682d7fd9e9b16d4a080
koomita <koomita@163.com>
2020-09-04 16:54:38 +0800

    fix：编辑器不修改image的url

--- src/components/tinymce.vue
+++ src/components/tinymce.vue
@@ -73,6 +73,7 @@ export default {
       browser_spellcheck: true, // 拼写检查
       branding: false, // 去水印
       extended_valid_elements: 'svg[*]',
+      convert_urls: false, // 如果您设置这个选项错误会尽量保持这些url完好无损。这个选项是默认设置为true,这意味着url将被迫是绝对或相对取决于relative_urls的状态。
       // elementpath: false,  //禁用编辑器底部的状态栏
       statusbar: false, // 隐藏编辑器底部的状态栏
       paste_data_images: true, // 允许粘贴图像
